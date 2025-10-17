fa87b5266cdb83ed7cba7700844319e7942aea9d
Leschwe1 <leandro.schweiss@gmail.com>
2025-10-18 00:17:53 +0200

    minor change in convertor

--- converter.py
+++ converter.py
@@ -201,7 +201,7 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
         ["Stammwürze",None,None,og-0.1,og,og+0.1,4,fermSteps[3]['name'],None,fermSteps[3]['temp'],fermSteps[3]['pressure'],"Flaschen 0.3",None,None,None,None],
         ["Restextrakt",None,None,round((fg-0.1),1),fg,round((fg+0.1),1),5,fermSteps[4]['name'],None,fermSteps[4]['temp'],fermSteps[4]['pressure'],"Direktausschank",None,None,None,None],
         ["pH" ,None,None,round((mashpH-1.5),2),round((mashpH-1.4),2),round((mashpH-1.3),2),6,fermSteps[5]['name'],None,fermSteps[5]['temp'],fermSteps[5]['pressure'],None,None,None,None,None],
-        ["Alkoholgehalt",None,None,round((abv-0.1),1),abv,round((abv+0.1),1),7,fermSteps[6]['name'],None,fermSteps[6]['temp'],fermSteps[6]['pressure'],'TOTAL' [l],None,None,None,None],
+        ["Alkoholgehalt",None,None,round((abv-0.1),1),abv,round((abv+0.1),1),7,fermSteps[6]['name'],None,fermSteps[6]['temp'],fermSteps[6]['pressure'],'TOTAL [l]',None,None,None,None],
         [None,"Datum","Zeit","°P","pH","Temp","soll","Druck","set","Truboff","Bemerkungen",None,None,None,None,"Initialen"], #ok
         [1 ,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
         [2 ,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
