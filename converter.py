cf09cd625fdec0b7ade1066be674651bef8ed765
Leschwe1 <leandro.schweiss@gmail.com>
2025-10-17 23:58:58 +0200

    converter.py: changed multi items to arrays

--- converter.py
+++ converter.py
@@ -30,10 +30,9 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
     ibu = data["ibu"]
 
     #line 5-11 ingredients list
-    malt_count = 0
-    for fermentable in data['data']['mashFermentables']:
-        malt_count += 1
-        exec("malt" + str(malt_count) + " = fermentable['name']")
+    malts = [fermentable['name'] for fermentable in data['data']['mashFermentables']]
+    while len(malts) < 7:
+        malts.append(None)
 
 
     hoplist = []
@@ -42,20 +41,14 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
         if hop['name'] not in hoplist:
             hoplist.append(hop['name'])
             alphalist.append(hop['alpha'])
-    hop_count = 0
-    for hop in hoplist:
-        hop_count += 1
-        exec("hop" + str(hop_count) + " = hop")
-    alpha_count = 0
-    for hopAlpha in alphalist:
-        alpha_count += 1
-        exec("hopAlpha" + str(alpha_count) + " = hopAlpha")
+    while len(hoplist) < 7:
+        hoplist.append(None)
+        alphalist.append(None)
 
 
-    yeast_count = 0
-    for yeast in data['yeasts']:
-        yeast_count += 1
-        exec("yeast" + str(yeast_count) + " = yeast['productId']")
+    yeasts = [y['productId'] for y in data['yeasts']]
+    while len(yeasts) < 7:
+        yeasts.append(None)
 
     #line 13-20 water profile
 
@@ -97,11 +90,9 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
     mashMaltAmount = round(float(data['data']["mashFermentablesAmount"]),1)
     Gussführung = f"1:{round(mashWaterAmount/mashMaltAmount, 1)}"
 
-    mashStep_count = 0
-    for mashStep in data['mash']['steps']:
-        mashStep_count += 1
-        exec("mashStepDur" + str(mashStep_count) + " = mashStep['stepTime']")
-        exec("mashStepTemp" + str(mashStep_count) + " = mashStep['stepTemp']")
+    mashSteps = [{'dur': step['stepTime'], 'temp': step['stepTemp']} for step in data['mash']['steps']]
+    while len(mashSteps) < 6:
+        mashSteps.append({'dur': None, 'temp': None})
 
 
     #line 29-35 lauter 
@@ -114,12 +105,9 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
     postBoilGravity = og
     postBoilVolume = round(data['equipment']["postBoilKettleVol"],1)
 
-    hopdose_count = 0
-    for hopdose in data['hops']:
-        hopdose_count += 1
-        exec("hopdose" + str(hopdose_count) + " = hopdose['name']")
-        exec("hopdoseTime" + str(hopdose_count) + " = hopdose['time']")
-        exec("hopdoseAmount" + str(hopdose_count) + " = hopdose['amount']")
+    hopdoses = [{'name': h['name'], 'time': h['time'], 'amount': h['amount']} for h in data['hops']]
+    while len(hopdoses) < 10:
+        hopdoses.append({'name': None, 'time': None, 'amount': None})
 
 
 
@@ -131,15 +119,17 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
     #fermsheet
     fermYeastAmount = data["yeasts"][0]["amount"]
 
-    fermStep_count = 0
-    for fermStep in data['fermentation']["steps"]:
-        fermStep_count += 1
-        pressureBar = 0
-        if fermStep['pressure'] is not None:
-            pressureBar = round(float(fermStep['pressure'])*0.0689476, 1)
-        exec("fermStepName" + str(fermStep_count) + " = fermStep['name']")
-        exec("fermStepTemp" + str(fermStep_count) + " = fermStep['stepTemp']")
-        exec("fermStepPressure" + str(fermStep_count) + " = pressureBar")
+    fermSteps = []
+    for step in data['fermentation']['steps']:
+        pressureBar = round(float(step['pressure'])*0.0689476, 1) if step['pressure'] is not None else 0
+        fermSteps.append({
+            'name': step['name'],
+            'temp': step['stepTemp'],
+            'pressure': pressureBar
+        })
+    # Pad to 5 ferm steps
+    while len(fermSteps) < 7:
+        fermSteps.append({'name': None, 'temp': None, 'pressure': None})
 
 
     #############################csv generation
@@ -149,13 +139,13 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
         [beerName ,None,None,None,batchno,None,None,None,brewdate,None,None,None,"Brewer:" ,None,None], #ok
         [style,None,og,"°P",abv,"%",fg,"°P",color,"EBC",ibu,"IBU",None,None,None], #ok
         ["ZUTATEN",None,"Prod","No",None,None,None,"alpha","Prod","No",None,None,None,"Prod","No"], #ok
-        [locals().get('malt1', None), None, None, None,None, locals().get('hop1', None), None, locals().get('hopAlpha1', None), None, None, None, locals().get('yeast1', None), None],
-        [locals().get('malt2', None), None, None, None,None, locals().get('hop2', None), None, locals().get('hopAlpha2', None), None, None, None, locals().get('yeast2', None), None],
-        [locals().get('malt3', None), None, None, None,None, locals().get('hop3', None), None, locals().get('hopAlpha3', None), None, None, None, locals().get('yeast3', None), None],
-        [locals().get('malt4', None), None, None, None,None, locals().get('hop4', None), None, locals().get('hopAlpha4', None), None, None, None, locals().get('yeast4', None), None],
-        [locals().get('malt5', None), None, None, None,None, locals().get('hop5', None), None, locals().get('hopAlpha5', None), None, None, None, locals().get('yeast5', None), None],
-        [locals().get('malt6', None), None, None, None,None, locals().get('hop6', None), None, locals().get('hopAlpha6', None), None, None, None, locals().get('yeast6', None), None],
-        [locals().get('malt7', None), None, None, None,None, locals().get('hop7', None), None, locals().get('hopAlpha7', None), None, None, None, locals().get('yeast7', None), None],
+        [malts[0], None, None, None,None, hoplist[0], None, alphalist[0], None, None, None, yeasts[0], None],
+        [malts[1], None, None, None,None, hoplist[1], None, alphalist[1], None, None, None, yeasts[1], None],
+        [malts[2], None, None, None,None, hoplist[2], None, alphalist[2], None, None, None, yeasts[2], None],
+        [malts[3], None, None, None,None, hoplist[3], None, alphalist[3], None, None, None, yeasts[3], None],
+        [malts[4], None, None, None,None, hoplist[4], None, alphalist[4], None, None, None, yeasts[4], None],
+        [malts[5], None, None, None,None, hoplist[5], None, alphalist[5], None, None, None, yeasts[5], None],
+        [malts[6], None, None, None,None, hoplist[6], None, alphalist[6], None, None, None, yeasts[6], None],
         ["WASSER",None,None,None,None,None,None,None,None,None,None,None,None,"Resp: ",None], #ok
         ["Maische [l]",None,mashWaterAmount,None,None,"Nachguss [l]",None,spargeWaterAmount,None,None,None,"Korrektur [l]",None,corrWaterAmount,None],
         ["Verdünnung [%]",None,mashDilution,None,None,"Verdünnung [%]",None,spargeDilution,None,None,None,"Verdünnung [%]",None,corrDilution,None],
@@ -166,12 +156,12 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
         ["NaHCO3 [g]",None,mashNaHCO3,None,None,"NaHCO3 [g]",None,spargeNaHCO3,None,None,None,"NaHCO3 [g]",None,corrNaHCO3,None],
         ["Lactic Acid 80% [ml]",None,mashLA,None,None,"Lactic Acid 80% [ml]",None,spargeLA,None,None,None,"Lactic Acid 80% [ml]",None,corrLA,None, None],
         ["MAISCHE",None,"act","min","soll","max",None,"°C","min","Notes",None,None,None,"Resp:" ,None],
-        ["pH (10mins) [pH]",None,None,mashpH - 0.1,mashpH,mashpH+0.1,"Step1",locals().get('mashStepTemp1', None),locals().get('mashStepDur1', None),None,None,None,None,None,None],
-        ["Korrektur LA [ml]",None,None,0,0,50,"Step2",locals().get('mashStepTemp2', None),locals().get('mashStepDur2', None),None,None,None,None,None,None],
-        [None,None,None,None,None,None,"Step3",locals().get('mashStepTemp3', None),locals().get('mashStepDur3', None),None,None,None,None,None,None],
-        ["Wasser [l]",None,None,mashWaterAmount - 10,mashWaterAmount,160,"Step4",locals().get('mashStepTemp4', None),locals().get('mashStepDur4', None),None,None,None,None,None,None],
-        ["Malz [kg]",None,None,None,mashMaltAmount,None,"Step5",locals().get('mashStepTemp5', None),locals().get('mashStepDur5', None),None,None,None,None,None,None],
-        ["Gussführung",None,None,None,Gussführung,None,"Step6",locals().get('mashStepTemp6', None),locals().get('mashStepDur6', None),None,None,None,None,None,None],
+        ["pH (10mins) [pH]",None,None,mashpH - 0.1,mashpH,mashpH+0.1,"Step1",mashSteps[0]['temp'], mashSteps[0]['dur'],None,None,None,None,None,None],
+        ["Korrektur LA [ml]",None,None,0,0,50,"Step2",mashSteps[1]['temp'], mashSteps[1]['dur'],None,None,None,None,None,None],
+        [None,None,None,None,None,None,"Step3",mashSteps[2]['temp'], mashSteps[2]['dur'],None,None,None,None,None,None],
+        ["Wasser [l]",None,None,mashWaterAmount - 10,mashWaterAmount,160,"Step4",mashSteps[3]['temp'], mashSteps[3]['dur'],None,None,None,None,None,None],
+        ["Malz [kg]",None,None,None,mashMaltAmount,None,"Step5",mashSteps[4]['temp'], mashSteps[4]['dur'],None,None,None,None,None,None],
+        ["Gussführung",None,None,None,Gussführung,None,"Step6",mashSteps[5]['temp'], mashSteps[5]['dur'],None,None,None,None,None,None],
         ["LÄUTERN",None,"act","min","soll","max",None,None,None,"Notes",None,None,None,"Resp:" ,None],
         ["Vorderwürze pH",None,None,mashpH - 0.1,mashpH,mashpH + 0.1,None,None,None,None,None,None,None,None,None],
         ["Vorderwürze °P",None,None,(og * 2)-3,og * 2,(og * 2)+3,None,None,None,None,None,None,None,None],
@@ -181,16 +171,16 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
         ["Glattwasser °P",None,None,3.0,5.0,7.0,None,None,None,None,None,None,None,None,None],
         [None,None,None,None,None,None,None,None,None,None,None,None,None], #ok
         ["KOCHEN",None,"act","min","soll","max","Sorte","Zeit","Menge","Notes",None,None,None,"Resp:" ,None],
-        ["preBoil pH",None,None,mashpH-0.2,mashpH,mashpH+0.2,locals().get('hopdose1',None),locals().get('hopdoseTime1',None),locals().get('hopdoseAmount1',None),None,None,None,None,None,None],
-        ["preBoil °P",None,None,preBoilGravity-0.1,preBoilGravity,preBoilGravity+0.1,locals().get('hopdose2',None),locals().get('hopdoseTime2',None),locals().get('hopdoseAmount2',None),None,None,None,None,None,None],
-        ["preBoil Volume",None,None,preBoilVolume-20,preBoilVolume,preBoilVolume+20,locals().get('hopdose3',None),locals().get('hopdoseTime3',None),locals().get('hopdoseAmount3',None),None,None,None,None,None,None],
-        ["preBoil LA [ml]",None,None,0,0,50,locals().get('hopdose4',None),locals().get('hopdoseTime4',None),locals().get('hopdoseAmount4',None),None,None,None,None,None,None],
-        [None,None,None,None,None,None,locals().get('hopdose5',None),locals().get('hopdoseTime5',None),locals().get('hopdoseAmount5',None),None,None,None,None,None,None],
-        ["postBoil pH",None,None,mashpH-0.5,mashpH-0.4,mashpH-0.3,locals().get('hopdose6',None),locals().get('hopdoseTime6',None),locals().get('hopdoseAmount6',None),None,None,None,None,None,None],
-        ["postBoil °P",None,None,og-0.1,og,og+0.1,locals().get('hopdose7',None),locals().get('hopdoseTime7',None),locals().get('hopdoseAmount7',None),None,None,None,None,None,None],
-        ["postBoil Volume",None,None,postBoilVolume - 10,postBoilVolume,postBoilVolume + 10,locals().get('hopdose8',None),locals().get('hopdoseTime8',None),locals().get('hopdoseAmount8',None),None,None,None,None,None,None],
-        ["postBoil LA [ml]",None,None,0,0,50,locals().get('hopdose9',None),locals().get('hopdoseTime9',None),locals().get('hopdoseAmount9',None),None,None,None,None,None,None],
-        [None,None,None,None,None,None,locals().get('hopdose10',None),locals().get('hopdoseTime10',None),locals().get('hopdoseAmount10',None),None,None,None,None,None,None],
+        ["preBoil pH",None,None,mashpH-0.2,mashpH,mashpH+0.2,hopdoses[0]['name'],hopdoses[0]['time'],hopdoses[0]['amount'],None,None,None,None,None,None],
+        ["preBoil °P",None,None,preBoilGravity-0.1,preBoilGravity,preBoilGravity+0.1,hopdoses[1]['name'],hopdoses[1]['time'],hopdoses[1]['amount'],None,None,None,None,None,None],
+        ["preBoil Volume",None,None,preBoilVolume-20,preBoilVolume,preBoilVolume+20,hopdoses[2]['name'],hopdoses[2]['time'],hopdoses[2]['amount'],None,None,None,None,None,None],
+        ["preBoil LA [ml]",None,None,0,0,50,hopdoses[3]['name'],hopdoses[3]['time'],hopdoses[3]['amount'],None,None,None,None,None,None],
+        [None,None,None,None,None,None,hopdoses[4]['name'],hopdoses[4]['time'],hopdoses[4]['amount'],None,None,None,None,None,None],
+        ["postBoil pH",None,None,mashpH-0.5,mashpH-0.4,mashpH-0.3,hopdoses[5]['name'],hopdoses[5]['time'],hopdoses[5]['amount'],None,None,None,None,None,None],
+        ["postBoil °P",None,None,og-0.1,og,og+0.1,hopdoses[6]['name'],hopdoses[6]['time'],hopdoses[6]['amount'],None,None,None,None,None,None],
+        ["postBoil Volume",None,None,postBoilVolume - 10,postBoilVolume,postBoilVolume + 10,hopdoses[7]['name'],hopdoses[7]['time'],hopdoses[7]['amount'],None,None,None,None,None,None],
+        ["postBoil LA [ml]",None,None,0,0,50,hopdoses[8]['name'],hopdoses[8]['time'],hopdoses[8]['amount'],None,None,None,None,None,None],
+        [None,None,None,None,None,None,hopdoses[9]['name'],hopdoses[9]['time'],hopdoses[9]['amount'],None,None,None,None,None,None],
         ["KÜHLEN",None,"act","min","soll","max",None,None,None,"Notes",None,None,None,"Resp:" ,None],
         ["Whirlpool [mins]",None,None,whirlpoolTime-5,whirlpoolTime,whirlpoolTime+5,None,None,None,None,None,None,None,None,None],
         ["Kühlen [mins]",None,None,15,20,25,None,None,None,None,None,None,None,None,None],
@@ -205,13 +195,13 @@ def converter(batchno, brewdate,in_path, out_path, excel_layout = 'brewsheet_emp
         [beerName ,None,None,None,batchno,None,None,None,brewdate,None,None,None,None ,None,None], #ok
         [style,None,og,"°P",abv,"%",fg,"°P",color,"EBC",ibu,"IBU",None,None,None], #ok
         ["FERMENTATION",None,"ist","min","soll","max","Schritt","Beding." ,None,"Temp","Druck","ABFÜLLUNG",None,"Resp:" ,None,None],
-        ["Hefemenge",None,None,None,fermYeastAmount,None,1,locals().get('fermStepName1',None),None,locals().get('fermStepTemp1',None),locals().get('fermStepPressure1',None),"Kegs 20l",None,None,None,None],
-        ["Gen",None,None,None,None,None,2,locals().get('fermStepName2',None),None,locals().get('fermStepTemp2',None),locals().get('fermStepPressure2',None),"Kegs 50l",None,None,None,None],
-        ["Viability",None,None,None,None,None,3,locals().get('fermStepName3',None),None,locals().get('fermStepTemp3',None),locals().get('fermStepPressure3',None),"Flaschen 0.5",None,None,None,None],
-        ["Stammwürze",None,None,og-0.1,og,og+0.1,4,locals().get('fermStepName4',None),None,locals().get('fermStepTemp4',None),locals().get('fermStepPressure4',None),"Flaschen 0.3",None,None,None,None],
-        ["Restextrakt",None,None,round((fg-0.1),1),fg,round((fg+0.1),1),5,locals().get('fermStepName5',None),None,locals().get('fermStepTemp5',None),locals().get('fermStepPressure5',None),"Direktausschank",None,None,None,None],
-        ["pH" ,None,None,round((mashpH-1.5),2),round((mashpH-1.4),2),round((mashpH-1.3),2),6,locals().get('fermStepName6',None),None,locals().get('fermStepTemp6',None),locals().get('fermStepPressure6',None),None,None,None,None,None],
-        ["Alkoholgehalt",None,None,round((abv-0.1),1),abv,round((abv+0.1),1),7,locals().get('fermStepName7',None),None,locals().get('fermStepTemp7',None),locals().get('fermStepPressure7',None),None,None,None,None,None],
+        ["Hefemenge",None,None,None,fermYeastAmount,None,1,fermSteps[0]['name'],None,fermSteps[0]['temp'],fermSteps[0]['pressure'],"Kegs 20l",None,None,None,None],
+        ["Gen",None,None,None,None,None,2,fermSteps[1]['name'],None,fermSteps[1]['temp'],fermSteps[1]['pressure'],"Kegs 50l",None,None,None,None],
+        ["Viability",None,None,None,None,None,3,fermSteps[2]['name'],None,fermSteps[2]['temp'],fermSteps[2]['pressure'],"Flaschen 0.5",None,None,None,None],
+        ["Stammwürze",None,None,og-0.1,og,og+0.1,4,fermSteps[3]['name'],None,fermSteps[3]['temp'],fermSteps[3]['pressure'],"Flaschen 0.3",None,None,None,None],
+        ["Restextrakt",None,None,round((fg-0.1),1),fg,round((fg+0.1),1),5,fermSteps[4]['name'],None,fermSteps[4]['temp'],fermSteps[4]['pressure'],"Direktausschank",None,None,None,None],
+        ["pH" ,None,None,round((mashpH-1.5),2),round((mashpH-1.4),2),round((mashpH-1.3),2),6,fermSteps[5]['name'],None,fermSteps[5]['temp'],fermSteps[5]['pressure'],None,None,None,None,None],
+        ["Alkoholgehalt",None,None,round((abv-0.1),1),abv,round((abv+0.1),1),7,fermSteps[6]['name'],None,fermSteps[6]['temp'],fermSteps[6]['pressure'],None,None,None,None,None],
         [None,"Datum","Zeit","°P","pH","Temp","soll","Druck","set","Truboff","Bemerkungen",None,None,None,None,"Initialen"], #ok
         [1 ,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
         [2 ,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
